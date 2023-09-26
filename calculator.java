b19607609111b21e695def481e1098f81a114abf
LikithaRaniDorai <likitharanidorai98@gmail.com>
2023-09-26 12:06:24 -0400

    first commit

--- calculator.java
+++ calculator.java
@@ -1,28 +1,32 @@
 import java.util.*;
 
 class Calculator{
+    /**
+     * @param args
+     */
     public static void main(String[] args){
         System.out.println("enter the operation number: ");
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println("Enter the numbers :");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        switch( n ){
+        try (Scanner sc = new Scanner(System.in)) {
+            int n=sc.nextInt();
+            System.out.println("Enter the numbers :");
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            switch( n ){
 
-            case 1:
-            System.out.println(a+b);
-            break;
-            case 2: 
-            System.out.println(a-b);
-            break;   
-            case 3 : 
-            System.out.println(a*b);
-                break; 
-            case 4 : 
-            System.out.println(a/b);
-                break;    
+                case 1:
+                System.out.println(a+b);
+                break;
+                case 2: 
+                System.out.println(a-b);
+                break;   
+                case 3 : 
+                System.out.println(a*b);
+                    break; 
+                case 4 : 
+                System.out.println(a/b);
+                    break;    
 
+            }
         }
     }
 }
